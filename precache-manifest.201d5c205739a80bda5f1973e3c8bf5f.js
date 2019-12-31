4d9baac72033d35b9d7252a8ee5908bf88bfe1b9
gregoryvalkovic <gregoryvalkovic@gmail.com>
2019-12-31 11:15:46 +1100

    Updates

---
+++ precache-manifest.201d5c205739a80bda5f1973e3c8bf5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a56520a6f5e60195ce6b64efa30069b7",
+    "url": "/trello_clone/index.html"
+  },
+  {
+    "revision": "6da1d843ec0479abbaa6",
+    "url": "/trello_clone/static/css/main.b4d423ff.chunk.css"
+  },
+  {
+    "revision": "e82f31034aa53aae09a9",
+    "url": "/trello_clone/static/js/2.8dd2cfcc.chunk.js"
+  },
+  {
+    "revision": "6da1d843ec0479abbaa6",
+    "url": "/trello_clone/static/js/main.c1e8f123.chunk.js"
+  },
+  {
+    "revision": "1c23407912b7cf345825",
+    "url": "/trello_clone/static/js/runtime-main.311b680f.js"
+  },
+  {
+    "revision": "3d30f44fa621b3cf813534cbc27682a9",
+    "url": "/trello_clone/static/media/background.3d30f44f.jpeg"
+  }
+]);
